e27ee8250860f749eba58faa0b977a02438f297f
Danylo-bee <61242093+Danylo-bee@users.noreply.github.com>
2024-10-01 16:45:01 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,4 +1,4 @@
-import numpy as np
+from numpy import sin, inf, arange
 
 # Параметри
 a, b = 1.1, 2.0  # Інтервал табулювання
@@ -6,18 +6,18 @@ h = 0.1          # Крок табулювання
 d = 0.001        # Допустима похибка
 
 # Функція для обчислення значення ряду
-def f_series(x, tolerance):
+def f_series(x, d):
     k = 1
     sum_value = 0
-    term = np.inf
-    while abs(term) > tolerance:
-        term = (1 / 2**k) * np.sin(x / 2**k)
+    term = inf
+    while abs(term) > d:
+        term = (1 / 2**k) * sin(x / 2**k)
         sum_value += term
         k += 1
-    return sum_value
+    return sum_value    
 
 # Табуляція
-x_values = np.arange(a, b + h, h)
+x_values = arange(a, b + h, h)
 f_values = [f_series(x, d) for x in x_values]
 
 # Виведення результатів у вигляді таблиці
