990d095d268b9505f0bbc6dee302da0553c83b88
Danylo-bee <61242093+Danylo-bee@users.noreply.github.com>
2024-11-06 23:31:35 +0200

    Lab4.py

--- entity.py
+++ entity.py
@@ -38,3 +38,23 @@ class Entity:
 
     def __repr__(self):
         return f"Entity('{self.__name}', {self.__age})"
+        
+    def main():
+    # Створення об'єктів класу Entity
+    entity1 = Entity("Danylo", 17)
+    entity2 = Entity("Artem", 21)
+    entity3 = Entity("Mykola", 35)
+
+    # Виведення значень всіх полів
+    print(entity1)
+    print(entity2)
+    print(entity3)
+
+    # Приклад роботи з методами доступу
+    entity1.set_name("UpdatedAlice")
+    entity1.set_age(26)
+    print(f"Updated entity1: {entity1}")
+
+# Виклик main методу
+if __name__ == "__main__":
+    main()
