1b682c445c5ddfbdbc04754fdf88ab9f5924e239
Danylo-bee <61242093+Danylo-bee@users.noreply.github.com>
2024-11-08 22:00:29 +0200

    Update entity.py

--- entity.py
+++ entity.py
@@ -58,17 +58,3 @@ class Entity:
 # Виклик main методу
 if __name__ == "__main__":
     main()
-    
-    class Stadium:
-    def __init__(self, spectators, name, lighting_power):
-        self.spectators = spectators 
-        self.name = name 
-        self.lighting_power = lighting_power 
-
-    def display_info(self):
-        print(f"Назва стадіону: {self.name}")
-        print(f"Кількість глядачів: {self.spectators}")
-        print(f"Потужність освітлення: {self.lighting_power} люксів")
-
-stadium = Stadium(50000, "Олімпійський", 1500)
-stadium.display_info()
